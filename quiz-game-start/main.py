001d247e04250e82dab3010e752c2e0cffee81ea
rehakui <khrnyk@gmail.com>
2026-05-12 23:03:28 +0900

    Create quiz project

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -4,9 +4,9 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+for question in question_data["results"]:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
@@ -15,4 +15,10 @@ for question in question_data:
 # print(question_bank[0].text)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{len(question_bank)}")
